f637f897d65e953516f94e84eb7d72c1200de3d3
Andreas Wilhelm <wilhelm@cqse.eu>
2024-10-17 11:06:16 +0200

    third commit

--- ft_0_simple_calls.cpp
+++ ft_0_simple_calls.cpp
@@ -1,11 +1,12 @@
 int renamed_global = 41;
+int a_new_global = 41;
 
 int foo(const int* arg) {
-  return *arg + renamed_global--;
+  return *arg + a_new_global + renamed_global--;
 }
 
 int bar(const int& arg) {
-   return foo(&arg) - renamed_global++;
+   return foo(&arg) - a_new_global - renamed_global++;
 }
 
 int main(int argc, char** argv) {
